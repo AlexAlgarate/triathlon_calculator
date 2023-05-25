db863576eec741b9478432c3b98aae53bc149239
Alex Algarate <alex.algarate@gmail.com>
2023-05-25 20:01:56 +0200

    wip

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 import unittest
 import tkinter as tk
-# from src.sports.swim import SwimCalculator
-from src.sports.run import RunCalculator
+from src.sports.swim import SwimCalculator
 
 
-class SwimCalculatorTest(unittest.TestCase):
+class TestSwimmCalculator(unittest.TestCase):
     def setUp(self) -> None:
         self.window = tk.Tk()
-        self.calculator = RunCalculator(self.window)
+        self.calculator = SwimCalculator(self.window, title="Swim Calculator")
 
     def tearDown(self) -> None:
         self.window.destroy()
@@ -21,23 +20,23 @@ class SwimCalculatorTest(unittest.TestCase):
         self.calculator._swim_pace()
 
         expected_result = "01:30 min/100mts"
-        actual_result = self.calculator.velocity_result.get()
-
-        self.assertEqual(actual_result, expected_result)
-
-    def test_swim_pace_invalid_input(self):
-        self.calculator.entry_fields["distance"].insert(0, "1000")
-        self.calculator.entry_fields["hour"].insert(0, "0")
-        self.calculator.entry_fields["minute"].insert(0, "15.2")
-        self.calculator.entry_fields["seconds"].insert(0, "0")
-
-        self.calculator._swim_pace()
-
-        expected_result = ""
         actual_result = self.calculator.speed_result.get()
 
         self.assertEqual(actual_result, expected_result)
-        self.assertTrue(self.calculator.speed_result.winfo_ismapped())
+
+    # def test_swim_pace_invalid_input(self):
+    #     self.calculator.entry_fields["distance"].insert(0, "1000")
+    #     self.calculator.entry_fields["hour"].insert(0, "0")
+    #     self.calculator.entry_fields["minute"].insert(0, "15.2")
+    #     self.calculator.entry_fields["seconds"].insert(0, "0")
+
+    #     self.calculator._swim_pace()
+
+    #     expected_result = ""
+    #     actual_result = self.calculator.speed_result.get()
+
+    #     self.assertEqual(actual_result, expected_result)
+    #     self.assertTrue(self.calculator.speed_result.winfo_ismapped())
 
 
 if __name__ == "__main__":
